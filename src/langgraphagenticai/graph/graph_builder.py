e5d742e67e70227b93b6ce49c3a1dec3d58d47a9
MKhurramPro <mkhurramshahzad33@gmail.com>
2026-09-23 22:40:11 +0500

    feat: implement chatbot node

--- src/langgraphagenticai/graph/graph_builder.py
+++ src/langgraphagenticai/graph/graph_builder.py
@@ -1,6 +1,6 @@
 from langgraph.graph import StateGraph, START, END
 from src.langgraphagenticai.state.state import State
-
+from src.langgraphagenticai.nodes.basic_chatbot_node import BasicChatbotNode
 
 
 class GraphBuilder:
@@ -17,7 +17,9 @@ class GraphBuilder:
         entry and exit point of the graph.
         """
 
-        self.graph_builder.add_node("chatbot", )
+        self.basic_chatbot_node = BasicChatbotNode(self.llm)
+
+        self.graph_builder.add_node("chatbot", self.basic_chatbot_node.process)
 
         self.graph_builder.add_edge(START,"chatbot")
         self.graph_builder.add_edge("chatbot",END)
