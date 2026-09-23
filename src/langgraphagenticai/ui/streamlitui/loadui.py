b4e9d2c1dfd598e5fed65a84a907bc78177ccfe2
MKhurramPro <mkhurramshahzad33@gmail.com>
2026-09-23 14:56:14 +0500

    feat: configure OpenAI API integration

--- src/langgraphagenticai/ui/streamlitui/loadui.py
+++ src/langgraphagenticai/ui/streamlitui/loadui.py
@@ -24,7 +24,7 @@ class LoadStreamlitUI:
             if self.user_controls["selected_llm"] == 'Openai':
                 # Model selection
                 model_options = self.config.get_openai_model_options()
-                self.user_controls["selected_groq_model"] = st.selectbox("Select Model", model_options)
+                self.user_controls["selected_openai_model"] = st.selectbox("Select Model", model_options)
                 self.user_controls["OPENAI_API_KEY"] = st.session_state["OPENAI_API_KEY"]=st.text_input("API Key",type="password")
                 # Validate API key
                 if not self.user_controls["OPENAI_API_KEY"]:
